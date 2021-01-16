49b4ac2315e2652e1c8f943e221975bebf52ef5f
gaoshengyi09018133 <1405205313@qq.com>
2021-01-16 22:14:29 +0800

    取消数据库连接

--- src/main/java/com/vvvb/game/GameApplication.java
+++ src/main/java/com/vvvb/game/GameApplication.java
@@ -1,10 +1,19 @@
 package com.vvvb.game;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        DruidDataSourceAutoConfigure.class ,
+        HibernateJpaAutoConfiguration.class})
 @MapperScan("com.vvvb.game.*.mapper")
 public class GameApplication {
 
